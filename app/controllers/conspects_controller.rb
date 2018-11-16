5c9cda29c76f04280718582c98e7fdaea0ef6bfa
Vol6tmetr <mythbusterpeter@gmail.com>
2018-11-16 21:17:26 +0300

    Only logged users can modify

--- app/controllers/conspects_controller.rb
+++ app/controllers/conspects_controller.rb
@@ -1,5 +1,6 @@
 class ConspectsController < ApplicationController
   before_action :set_conspect, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:index, :show]
 
   # GET /conspects
   # GET /conspects.json
